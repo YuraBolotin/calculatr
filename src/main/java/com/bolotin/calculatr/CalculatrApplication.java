cdba0fe4c14300d5ad8b66ae421f8cfca84ff7e2
Yura <bolotinya@mail.ru>
2022-05-02 23:45:15 +0300

    Домашнее задание калькулятор

---
+++ src/main/java/com/bolotin/calculatr/CalculatrApplication.java
@@ -0,0 +1,13 @@
+package com.bolotin.calculatr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatrApplication.class, args);
+    }
+
+}
